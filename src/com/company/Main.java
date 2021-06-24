a096eb13dcc83aea01aed7db324d649b50a040af
RigaAV <vasiljevs.a@inbox.lv>
2021-06-24 21:32:56 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+//Classes
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person vitya = new Person();
+        vitya.say("George");
+        System.out.print(vitya.height);
+        Person katja = new Person(123);
+        System.out.print(katja.height);
+    }
+}
+
